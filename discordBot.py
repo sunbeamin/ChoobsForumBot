3c3cd3e5807ae764e52980b837032fdb6556ed6b
Marijn Stam <marijnstam@betonic.nl>
2022-01-08 00:40:40 +0100

    bot: add the task for sending new messages to discord
    collect info from scraper and format before sending. polled once every minute.

--- discordBot.py
+++ discordBot.py
@@ -1,5 +1,7 @@
 import discord
+from discord.ext import commands
 import os
+import asyncio
 from dotenv import load_dotenv
 
 import forumScraper
@@ -7,21 +9,43 @@ import forumScraper
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
+initComplete = False
 
-results = forumScraper.getLatestForumPost()
-print(results.forumURL)
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
 
+async def pollForum():
+    await client.wait_until_ready()
 
-# @client.event
-# async def on_ready():
-#     print('We have logged in as {0.user}'.format(client))
+    while True:
+        print("Checking forum...")
+        results = forumScraper.getLatestForumPost()
 
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
+        #The results object will only hold data if the scraper has retrieved a new message
+        if(results.forumPost != None):
+            print("Choobs Forum Bot is sending a message!")
+            #Retrieve the channel we want to send to. Replace this with the ID of the desired Discord channel
+            channel = client.get_channel(929118558963761153)
+            
+            #Embed the data into a nice format
+            embed=discord.Embed(
+            title="Iron Choobs Forum",
+                url=results.forumURL,
+                description="A new post has been placed on the Iron Choobs forum!",
+                color=discord.Color.blue())
+            embed.set_thumbnail(url="https://ws.shoutcast.com/images/contacts/0/07a6/07a648bc-68cb-4ad5-aadb-bf118339abdd/radios/c0cd2c27-a667-4275-82b8-2a744b66ca62/c0cd2c27-a667-4275-82b8-2a744b66ca62.png")
+            embed.add_field(name="**User**", value=results.username, inline=False)
+            embed.add_field(name="**Timestamp**", value=results.timestamp, inline=False)
+            embed.add_field(name="**Post content**", value=f"```{results.forumPost} ```", inline=False)
+            embed.add_field(name="**URL**", value=results.forumURL, inline=False)
+            embed.set_footer(text="If my bot is acting up, DM me @sun beamin")
 
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
+            await channel.send(embed=embed)
 
-# client.run(TOKEN)
+        await asyncio.sleep(60)
+
+if __name__ == "__main__":
+    client.loop.create_task(pollForum())
+    client.run(TOKEN)
+
--- forumScraper.py
+++ forumScraper.py
@@ -3,9 +3,11 @@ from bs4 import BeautifulSoup
 import urllib.request
 
 class returnObject:
-    def __init__(self, forumURL = None, forumPost = None):
+    def __init__(self, forumURL = None, forumPost = None, username = None, timestamp = None):
       self.forumURL = forumURL
       self.forumPost = forumPost
+      self.username = username
+      self.timestamp = timestamp
 
 def getLatestForumPost():
 
@@ -13,8 +15,13 @@ def getLatestForumPost():
 
     # Get the HTML of the Iron Choobs forum page
     forumURL = 'https://secure.runescape.com/m=forum/c=zaAuPnMkWRg/forums?320,321,155,66237297,goto,'
-    req = urllib.request.Request(url=forumURL, headers={'User-Agent': 'Mozilla/5.0'})
-    html = urllib.request.urlopen(req).read()
+    try:
+        req = urllib.request.Request(url=forumURL, headers={'User-Agent': 'Mozilla/5.0'})
+        html = urllib.request.urlopen(req).read()
+    except urllib.error.HTTPError as e:
+        print(e.__dict__)
+    except urllib.error.URLError as e:
+        print(e.__dict__)
 
     soup = BeautifulSoup(html, features="html.parser")
 
@@ -23,24 +30,37 @@ def getLatestForumPost():
 
     # Retrieve the HTML of the last page on the forum
     forumLastPageURL = forumURL + maxPaginationNumber
-    req = urllib.request.Request(url=forumLastPageURL, headers={'User-Agent': 'Mozilla/5.0'})
-    html = urllib.request.urlopen(req).read()
+    
+    try:
+        req = urllib.request.Request(url=forumLastPageURL, headers={'User-Agent': 'Mozilla/5.0'})
+        html = urllib.request.urlopen(req).read()
+    except urllib.error.HTTPError as e:
+        print(e.__dict__)
+    except urllib.error.URLError as e:
+        print(e.__dict__)
 
     soup = BeautifulSoup(html, features="html.parser")
 
-    #Find the timestamp of the latest entry on the latest page 
+    # Find the timestamp of the latest entry on the latest page 
     latestReplyTimestamp = soup.findAll('p', class_="forum-post__time-below")[-1].get_text()
-    fp = open(r"PostTimestamp.txt", "r")
-    latestReplySaved = fp.read()
+    with open(r"PostTimestamp.txt", "r") as fp:
+        latestReplySaved = fp.read()
 
     #Check if the timestamp matches that of the previously fetched latest message
     #If these do not match, it means a new message has been posted. Save the new timestamp
-    # if(latestReplyTimestamp != latestReplySaved):
-    fp = open(r"PostTimestamp.txt", "w+")
-    fp.write(latestReplyTimestamp)
+  
+    if(latestReplyTimestamp != latestReplySaved):
 
-    forumData.forumURL = forumLastPageURL
-    forumData.forumPost = soup.findAll('span', class_="forum-post__body")[-1].get_text()
+        with open(r"PostTimestamp.txt", "w+") as fp:
+            fp.write(latestReplyTimestamp)
+            fp.close()
+
+        #Set our object variables, these stay as None otherwise
+        forumData.forumURL = forumLastPageURL
+        forumData.forumPost = soup.findAll('span', class_="forum-post__body")[-1].get_text()
+        forumData.username = soup.findAll('a', class_="post-avatar__name-link")[-1].get_text()
+        forumData.timestamp = latestReplyTimestamp
+
     return forumData
 
 
